1e6d4921203ce96063007801e59e460f365ba624
iceanfire <iceanfire@gmail.com>
2013-11-11 00:57:48 +0000

    deleted some pointless old code especially code in main.py that already existed in setup.py

--- main.py
+++ main.py
@@ -42,97 +42,7 @@ class authHandler(webapp2.RequestHandler):
 		m.update(longStr)
 		return m.hexdigest()
 
-class getShoeScribe(authHandler):
-	def post(self):
-#		url = "http://pf.tradedoubler.com/export/export?myFeed=13814257482332345&myFormat=13814257482332345"
-#		url = "http://pf.tradedoubler.com/export/export?myFeed=13814286512332345&myFormat=13814286512332345";
-		#url = "http://pf.tradedoubler.com/export/export?myFeed=13816649672332345&myFormat=13814286512332345"
-	  	#result = urllib2.urlopen(url).read()
-		#output = StringIO.StringIO(result)
-
-
-		i=0
-		k=0
-		tableRows = []
-		category = []
-	
-		shoeData = open('data/data.csv','rb')
-		shoeData = shoeData.read()
-		shoeData = StringIO.StringIO(shoeData)
-
-		cr = csv.reader(shoeData)
-
-		for row in cr:
-			rowStr = ' '.join(row)
-
-			colArray = rowStr.split("|")
-			
-			if i>0:
-				colArray = [v.replace('"','') for v in colArray]
-				addNewShoe = shoes2()
-
-				if colArray[30]=="YOOX_direct UK":
-					addNewShoe.source="YOOX"
-				else:
-					addNewShoe.source = "ShoeScribe"
-
-				addNewShoe.name = colArray[0]
-				addNewShoe.url = colArray[1]
-				addNewShoe.description = colArray[3]
-				addNewShoe.price = float(colArray[4])
-				addNewShoe.currency = colArray[5]
-				addNewShoe.pId = int(colArray[6])
-				addNewShoe.catId = int(colArray[7])
-				addNewShoe.catName = colArray[8]
-				addNewShoe.mcatName = colArray[9]
-				addNewShoe.sku = colArray[10]
-
-				#get category name
-				if(addNewShoe.mcatName in category):
-					pass
-				else:
-					category.append(addNewShoe.mcatName)
-
-				try:
-					addNewShoe.prevPrice = float(colArray[13])
-				except:
-					addNewShoe.prevPrice = addNewShoe.price
-
-				shoeSizes = colArray[20].split(" ")
-				addNewShoe.sizes = [v.replace('.0','') for v in shoeSizes]
-
-				addNewShoe.inStock = True #this becomes an if/else statement
-
-				#add field data here		
-				fieldData = colArray[34].replace('"','').split(";")
-
-				for field in fieldData:
-					field = field.split(":")
-					if field[0]=="color":
-						addNewShoe.color = field[1]
-					if field[0]=="largeimage":
-						addNewShoe.img = "http:"+field[2]
-					if field[0]=="sex":
-						if field[1]=="MEN":
-							addNewShoe.sex = True
-						else:
-							addNewShoe.sex = False
-
-				addNewShoe.put() #I have disabled puts for security purposes
-			i=i+1
-		logging.info(category)
-#		template_values = {}
-#		template_values['tableRows']=tableRows
-#		self.render_response('index.html', **template_values)
-
-class setTask(authHandler):
-	def get(self):
-		self.response.write('<form method="post" action="/admin/new/task"><input type="submit"></form>')
-	def post(self):
-		taskqueue.add(url='/admin/new/feed/shoeScribe')
-
 class getShoes(authHandler):
-
 	def get(self):
 		template_values = {}
 		self.render_response('getDemo.html', **template_values)
@@ -254,8 +164,6 @@ class getShoes(authHandler):
 
 class getCat(authHandler):
 	def get(self):
-
-
 		catList = ["Sandals","Heels","Flats","Boots"]
 		colorList = {'Red':'#xxxxx','Black':'#xxxxx','Brown':'#xxxxx','Green':'#xxxxx','Grey':'#xxxxx','Orange':'#xxxxx','Purple':'#xxxxx','Yellow':'#xxxxx','Blue':'#xxxxx','Pink':'#xxxxx','White':'#xxxxx'}
 		priceList = ['Any Price','Under £20','£21-100','£101-200','£201-500','£501+']
@@ -263,9 +171,8 @@ class getCat(authHandler):
 		self.response.headers['Content-Type'] = 'application/json'
 		self.response.out.write(json.dumps(categories))
 
-class pushAction(authHandler):
+class pushAction(authHandler): #Manages updates of likes/dislikes
 	def get(self):
-
 		template_values = {}
 		self.render_response('actDemo.html', **template_values)
 
@@ -354,65 +261,6 @@ class queueAction(authHandler):
 			else:
 				logging.warning(devMode)
 
-class updateCat(authHandler):
-	def get(self):
-		self.response.write('disabled!')
-		#taskqueue.add(url="/admin/update/cat")
-	def post(self):
-		getShoes = shoes2.query()
-		results = getShoes.fetch()
-		i = 0
-		for result in results:
-			i = i+1
-			key = result.key.urlsafe()
-			#taskqueue.add(url="/admin/update/cat/task", params={'counter':i,'key':key})
-
-class updateCatTask(authHandler):
-	def post(self):
-		counter = self.request.get('counter')
-		logging.info(counter)
-		key = self.request.get('key')
-		entity = ndb.Key(urlsafe=key).get()
-		mapping = {
-			'Boots_Ankle boots_D':'Boots',
-			'Boots_Boots_D':'Boots',
-			'Espadrilles_Espadrilles_D':'NA',
-			'Extras_Shoecare_D':'NA',
-			'FOOTWEAR_Ankle boots_D':'Boots',
-			'FOOTWEAR_Ankle boots_U':'Boots',
-			'FOOTWEAR_Ballet flats_D':'Flats',
-			'FOOTWEAR_Boots_D':'Boots',
-			'FOOTWEAR_Boots_U':'NA',
-			'FOOTWEAR_Clog sandals_D':'NA',
-			'FOOTWEAR_Closed-toe slip-ons _D':'Heels',
-			'FOOTWEAR_Combat boots_D':'NA',
-			'FOOTWEAR_Combat boots_U':'Boots',
-			'FOOTWEAR_Flip flops_D':'NA',
-			'FOOTWEAR_Flip flops_U':'NA',
-			'FOOTWEAR_High-heeled boots_D':'Boots',
-			'FOOTWEAR_High-heeled sandals_D':'Heels',
-			'FOOTWEAR_High-top dress shoes_U':'NA',
-			'FOOTWEAR_Moccasins with heel_D':'Heels',
-			'FOOTWEAR_Mules_D':'Boots',
-			'FOOTWEAR_Peep-toe ballet flats_D':'NA',
-			'FOOTWEAR_Platform sandals_D':'Heels',
-			'FOOTWEAR_Sandals_D':'Sandals',
-			'FOOTWEAR_Sandals_U':'NA',
-			'FOOTWEAR_Shoe boots_D':'Boots',
-			'FOOTWEAR_Slingbacks_D':'Heels',
-			'FOOTWEAR_Slippers_D':'NA',
-			'FOOTWEAR_Slippers_U':'NA',
-			'FOOTWEAR_Wedges_D':'Heels'
-		}
-		#logging.info(entity.sCat)
-		if(entity.sCat):
-			pass
-		else:
-			if entity.mcatName in mapping:
-				entity.sCat = mapping[entity.mcatName]
-				entity.put()
-			else:
-				pass
 
 class pushEmail(authHandler):
 	def post(self):
@@ -497,34 +345,6 @@ class newUser(authHandler):
 	def get(self):
 		template_values = {}
 		self.render_response('userDemo.html', **template_values)
-
-		#import uuid
-		#uID = self.request.get('uuid')
-		#status = {}
-
-		#if(uID!=""):
-		#	#uID = uuid.UUID(uID).int
-		#	newUser = userData(id=uID)
-		#	newUser.uuId = uID
-		#	newUser.lastUpdate = datetime.datetime.now()
-
-			#check if newUser already exists
-		#	q = userData.query(userData.uuId==uID)
-		#	results = q.fetch(1)
-		#	if len(results)>0:
-		#		status = {'status':'Failed','Reason':'UUID already Exists'}
-		#	else:
-		#		try:
-		#			newUser.put()
-		#			status = {'status':'OK'}
-		#		except:
-		#			status = {'status':'Failed','Reason':'Unknown'}
-	#	else:
-	#		status = {'status':'Failed','reason':'No uuid provided'}
-#
-#		self.response.headers['Content-Type'] = 'application/json'
-#		self.response.out.write(json.dumps(status))
-
 
 application = webapp2.WSGIApplication([
 	('/admin/new/task',setTask),
